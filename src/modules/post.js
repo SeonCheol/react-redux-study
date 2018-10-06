d3b4ca5b7fe6415b560666106c96b3a2ada9121c
seoncheol.kim <seoncheol.kim@navercorp.com>
2018-10-06 14:15:57 +0900

    fetch post using redux-thunk

---
+++ src/modules/post.js
@@ -0,0 +1,52 @@
+import {handleActions, createAction} from 'redux-actions'
+import {Map} from 'immutable'
+import axios from 'axios'
+
+function getPostAPI(postId) {
+    return axios.get(`http://jsonplaceholder.typicode.com/posts/${postId}`)
+}
+
+const GET_POST_PENDING = 'GET_POST_PENDING'
+const GET_POST_SUCCESS = 'GET_POST_SUCCESS'
+const GET_POST_FAILURE = 'GET_POST_FAILURE'
+
+const getPostPending = createAction(GET_POST_PENDING)
+const getPostSuccess = createAction(GET_POST_SUCCESS)
+const getPostFailure = createAction(GET_POST_FAILURE)
+
+export const getPost = (postId) => dispatch => {
+    dispatch(getPostPending())
+
+    return getPostAPI(postId).then((response) => {
+        dispatch(getPostSuccess(response))
+        return response
+    }).catch(err => {
+        dispatch(getPostFailure(err))
+        throw err
+    })
+}
+
+const initialState = Map({
+    pending: false,
+    error: false,
+    data: {
+        title: '',
+        body: '',
+    }
+})
+
+export default handleActions({
+    [GET_POST_PENDING]: (state, action) => {
+        return state.set('pending', true)
+    },
+    [GET_POST_SUCCESS]: (state, action) => {
+        const {title, body} = action.payload.data
+        const data = {title, body}
+        return state.set('data', data)
+            .set('pending', false)
+    },
+    [GET_POST_FAILURE]: (state, action) => {
+        return state.set('pending', false)
+            .set('error', true)
+    }
+}, initialState)
